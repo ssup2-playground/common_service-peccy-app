8a6f9c8416666f2caf903f4e35ee6e1533856a2e
ssup2 <supsup5642@gmail.com>
2023-02-16 10:49:11 +0900

    Add DB creation logic

--- domain/repo/__init__.py
+++ domain/repo/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 db_username = os.environ.get("MYSQL_USER", "root")
 db_password = os.environ.get("MYSQL_PASSWORD", "root")
@@ -10,8 +11,11 @@ db_port = os.environ.get("MYSQL_PORT", "3306")
 db_name = os.environ.get("MYSQL_DB_NAME", "peccy")
 
 engine = create_engine(
-    f"mysql+mysqldb://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}", pool_pre_ping=True
+    f"mysql+mysqldb://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}",
+    pool_pre_ping=True,
 )
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
